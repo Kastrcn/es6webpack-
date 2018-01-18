028e2ee22979b57c52e60cb2019bede0ea56ca0b
Kastrcn <Kastrcn@WEIYOU-PRO>
2018-01-18 11:38:29 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    //扩展插件
+    "extends": "airbnb",
+    "rules": {
+        // 0 = off, 1 = warn, 2 = error 
+        //行尾缺少分号可能导致一些意外情况
+        "no-unexpected-multiline": 2,
+        //4个空格开头
+        "indent": [2, 4],
+        //语句强制不分号结尾
+        "semi": [2, "never"],
+        //换行风格
+        'linebreak-style': ["off", "windows"],
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-var array = [1, 2, 3];
-//传统写法
-array.forEach(function(v, i, a) {
-    console.log(v);
-});
-//ES6
-array.forEach(v => console.log(v));
+const array = [1, 2, 3]
+// 传统写法
+// eslint不进行代码检测格式检测注释
+/* eslint prefer-arrow-callback: "off" */
+array.forEach(function e(v) {
+    /* eslint-disable no-console */
+    console.log(v)
+})
+// ES6
+// eslint 不进行代码检测格式检测注释
+/* eslint-disable no-console */
+array.forEach(v => console.log(v))
